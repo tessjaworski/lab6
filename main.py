4ff78564d92be8e25b40f929e54340b17b1eab4e
jesse-mej <jessemejia@ufl.edu>
2023-10-31 12:56:25 -0400

    finished decode + main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#Tess Jaworski
+# Tess Jaworski
 def print_menu():
     print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit')
+    print()
+
 
 def encode(user_pass):
     new_pass = ""
@@ -11,11 +13,27 @@ def encode(user_pass):
     return new_pass
 
 
+def decode(password):
+    final_password = ""
+    for i in password:
+        shifted_num = (int(i) + 7)  # add 7 to handle it wrapping around (same thing as -3 and +10)
+        final_num = shifted_num % 10
+        final_password += str(final_num)
+    return final_password
+
+
 if __name__ == "__main__":
     while True:
         print_menu()
-        user_input = input('Please enter an option:')
+        user_password = 0
+        user_input = input('Please enter an option: ')
         if user_input == '1':
-            user_pass = input('Please enter your password to encode:')
-            encode(user_pass)
-
+            user_password = input('Please enter your password to encode: ')
+            final_pass = encode(user_password)
+            print("Your password has been encoded and stored!")
+            print()
+        if user_input == '2':
+            print(f"The encoded password is {final_pass}, and the original password is {decode(final_pass)}.")
+            print()
+        if user_input == '3':
+            break
